d9564f5311eccc7b751c61e84cf41ed80a386db7
kareemamursy2007 <kareemmursy93@gmail.com>
2025-11-11 09:06:16 +0200

    Implement Person class with Display method
    Added Person class with private members and Display method.

--- Person.h
+++ Person.h
@@ -3,8 +3,18 @@
 
 #include <string>
 using namespace std;
-
-//#write Person class here
+person class{
+  private:
+  String Name;
+  int ID;
+public:
+person Display(string Name , int ID){
+ this Name-> Name ;
+this ID -> ID ; 
+cout<< "The id is : " << ID ;
+cout<< "The Name is : " << Name ;
+}
+};
 
 
 
