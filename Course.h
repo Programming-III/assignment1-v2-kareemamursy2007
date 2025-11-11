525af9d596ff9ea0a17a9f5eafff95a3c22b4c7e
kareemamursy2007 <kareemmursy93@gmail.com>
2025-11-11 09:30:57 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -11,7 +11,32 @@ course class{
           int MaxStudents;
           Student* Students[];
           int CurrentStudents;
-
+          addStudent();
+          DisplayCourseInfo();
+ public:
+         course addstudent::Student(const student& s){
+           this student -> student[];
+           student[] = new student ; 
+}
+      Display CourseCode(string CourseCode){
+      this CourseCode -> CourseCoded;
+      return CourseCode;
+}
+    Display CourseName(string CourseName){
+    this CoursesName -> CourseName;
+    return CourseName;
+}
+    DisplayCourseInfo(string CourseCode , string  CourseName , int  MaxStudents , int CurrentStudents){
+    this MaxStudents -> MaxStudents; 
+    this CurrentStudents -> CurrentStudents;
+    cout << "The CourseCode is : " << CourseCode;
+    cout << "The CourseName is : " << CourseName;
+    cout << " The Number of Max Stuents is : " << MaxStudents :
+    cout << "The number of current Students is : " << CurrentStudents;\
+}
+};
+    
+           
 
 
 
