6d3aa769aabf6f0ccf22f4bca05d6e06db23d685
kareemamursy2007 <kareemmursy93@gmail.com>
2025-11-11 09:03:33 +0200

    Implement person and Student classes
    Added person and Student classes with Display methods.

--- Course.h
+++ Course.h
@@ -5,7 +5,25 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
-
+person class{
+  private:
+  String Name;
+  int ID;
+public:
+person Display(string Name , int ID){
+ this Name-> Name ;
+this ID -> ID ; 
+cout<< "The id is : " << ID ;
+cout<< "The Name is : " << Name ;
+}
+};
+Student class{
+  private:
+          int YearLevel;
+          string Major;
+  public:
+      Student Display(int YearLevel , string Major
+       
 
 
 
